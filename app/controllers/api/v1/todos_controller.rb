e504196b0cda4f7bdeb79ed6e2e620de81f95e8e
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-12 00:44:25 +0900

    1011_todos_controller.rb_各アクションの設定 #1

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,30 +1,43 @@
 class Api::V1::TodosController < ApplicationController
 
   def show
-    render json: { status: 200, todos: Todo.find(params[:id]) }
+    # render json: { status: 200, todos: Todo.find(params[:id]) }
+    @todo = Todo.find(params[:id])
+    render "show.json.jb"
   end
 
   def index
-    render json: { status: 200, todos: Todo.all }
+    # render json: { status: 200, todos: Todo.all }
+    @todos = Todo.all
+    render "index.json.jb"
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
   end
 
   def create
-    todo = Todo.new(todo_params)
+    # @todo = Todo.find(params[:team_id])
+    @todo = Todo.new(todo_params)
 
-    if todo.save
-      render json: { status: 200, todo: todo }
+    if @todo.save
+      render "index.json.jb"
+      # render json: { status: 200, todo: todo }
     else
-      render json: { status: 500, message: "Todoの作成に失敗しました" }
+      render "error.json.jb"
+      # render json: { status: 500, message: "Todoの作成に失敗しました" }
     end
   end
 
   def destroy
-    todo = Todo.find(params[:id])
+    @todo = Todo.find(params[:id])
 
-    if todo.destroy
-      render json: { status: 200, todo: todo }
+    if @todo.destroy
+      render "index.json.jb"
+      # render json: { status: 200, todo: todo }
     else
-      render json: { status: 500, message: "Todoの削除に失敗しました" }
+      render "error.json.jb"
+      # render json: { status: 500, message: "Todoの削除に失敗しました" }
     end
   end
 
