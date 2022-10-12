61b4242a9283b49885cc7e27fdc760abd59f65fc
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-12 21:16:24 +0900

    1012_不要なコメントアウトの削除 #1

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::TodosController < ApplicationController
 
   def show
-    # render json: { status: 200, todos: Todo.find(params[:id]) }
     @todo = Todo.find(params[:id])
     render "show.json.jb"
   end
 
   def index
-    # render json: { status: 200, todos: Todo.all }
     @todos = Todo.all
     render "index.json.jb"
   end
@@ -17,11 +15,9 @@ class Api::V1::TodosController < ApplicationController
   end
 
   def create
-    # @todo = Todo.find(params[:team_id])
     @todo = Todo.new(todo_params)
 
     if @todo.save
-      # render "index.json.jb"
       render json: { status: 200 }
     else
       render "error.json.jb"
