43ee1b72a0f519d9c417142ec33fa59f2a9daa60
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-12 21:42:43 +0900

    1012_errorのステータスを固定していたものを削除 #1

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -16,21 +16,15 @@ class Api::V1::TodosController < ApplicationController
 
   def create
     @todo = Todo.new(todo_params)
-
     if @todo.save
       render json: { status: 200 }
-    else
-      render json: { status: 500 }
     end
   end
 
   def destroy
     @todo = Todo.find(params[:id])
-
     if @todo.destroy
       render json: { status: 200 }
-    else
-      render json: { status: 500 }
     end
   end
 
