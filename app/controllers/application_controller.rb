7ac58e5c0f46da0c459f0ac6953cab49861d752c
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-17 00:07:22 +0900

    1017_不要な引数(e)を削除 #4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound,   with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
 
-  def render_404(e)
+  def render_404
     render json: { status: 404 }
   end
 
-  def render_500(e)
+  def render_500
     render json: { status: 500 }
   end
 
