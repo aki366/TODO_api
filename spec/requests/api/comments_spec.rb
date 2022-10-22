4d8ca5faaeebb4fb9bdae64c0065be5e6cab0641
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-22 22:43:04 +0900

    requests_comments_テストケース作成 #17

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -1,7 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  describe '正常系' do
+    describe 'GET #show' do
+      context 'TODOに紐づくCommentにアクセスしたとき' do
+        it 'statusが200になること' do
+        end
+        it 'responseの中身が正しいこと' do
+        end
+      end
+    end
+
+    describe 'GET #index' do
+      context 'TODOに紐づく複数のCommentがあるとき' do
+        it 'statusが200になること' do
+        end
+        it 'responseの中身が正しいこと' do
+        end
+      end
+    end
+
+    describe 'PUT #update' do
+      context 'Commentを編集したとき' do
+        it 'responsが正常であること' do
+        end
+      end
+    end
+
+    describe 'POST #create' do
+      context '存在するTODOに紐づくCommentを作成したとき' do
+        it 'statusが200になること' do
+        end
+
+        it 'responseの中身が正しいこと' do
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      context 'Commentを削除したとき' do
+        it 'statusが200になること' do
+        end
+
+        it 'Commentが削除できること' do
+        end
+      end
+
+      context 'Commentに紐づくTODOが削除されたとき' do
+        it 'statusが200になること' do
+        end
+
+        it 'Commentが削除されること' do
+        end
+      end
+    end
+  end
+
+  describe '異常系' do
+    describe 'GET #show' do
+      context 'レコードが存在しない場合' do
+        it 'statusが404になること' do
+        end
+      end
+
+      context 'Excepionエラーの場合' do
+        it 'statusが500になること' do
+        end
+      end
+    end
+
+    describe 'GET #index' do
+      context 'Excepionエラーの場合' do
+        it 'statusが500になること' do
+        end
+      end
+    end
+
+    describe 'PUT #update' do
+      context 'レコードが存在しない場合' do
+        it 'statusが404になること' do
+        end
+      end
+
+      context 'Excepionエラーの場合' do
+        it 'statusが500になること' do
+        end
+      end
+    end
+
+    describe 'POST #create' do
+      context 'Excepionエラーの場合' do
+        it 'statusが500になること' do
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      context 'レコードが存在しない場合' do
+        it 'statusが404になること' do
+        end
+      end
+
+      context 'Excepionエラーの場合' do
+        it 'statusが500になること' do
+        end
+      end
+    end
   end
 end
