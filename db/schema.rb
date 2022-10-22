016bd538531640b638c33ec87d63dd0e2d5f2ef1
aki366 <96239914+aki366@users.noreply.github.com>
2022-10-22 15:49:39 +0900

    comments_db_migrate #16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_08_063914) do
+ActiveRecord::Schema.define(version: 2022_10_22_061544) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.integer "todo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_id"], name: "index_comments_on_todo_id"
+  end
 
   create_table "todos", force: :cascade do |t|
     t.text "text"
@@ -18,4 +26,5 @@ ActiveRecord::Schema.define(version: 2022_10_08_063914) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "todos"
 end
